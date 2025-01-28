fd4a1e19c22f361f4fbde2bec0810da2d11a6674
Alyx Dev <hyflicker@gmail.com>
2025-01-27 23:27:30 -0500

    Update marbles.js

--- marbles.js
+++ marbles.js
@@ -1,28 +1,30 @@
-import { Client } from "tmi.js";
-import "dotenv/config";
-var prefix = "!";
-export const client = new Client({
-	options: { debug: true },
+import { Client } from "tmi.js"; //Imports the Client Object from tmi.js package (Use NPM install tmi.js)
+import "dotenv/config"; //Imports the dotenv package for environment variables (Use NPM install dotenv)
+
+const client = new Client({ 
+	options: { debug: true }, 
 	identity: {
-		username: process.env.botuser,
-		password: process.env.botpass,
+		username: process.env.botuser, //Grabs the bots name dotenv file
+		password: process.env.botpass, //Grabs the bots oauth  dotenv file
 	},
-	channels: ["doctorsmooth"],
+	channels: ["doctorsmooth"], //Array of channels you want your bot to join
 });
 
+//Starts the bot by connecting client to twitch IRC
 client.connect()
 .then(() => {
 	console.log("Bot Online!")
 })
 
+// Global variable
 let playing = false;
 
+// client on function to listen to messages
 client.on("message", async (channel, tags, message, self) => {
-	if (self) return;
+	if (self) return; // This ignores messages from yourself.
 	var args = message.slice(1, message.length).split(" ");
-
+	
 	if(args[0] === "play"){
-		// console.log(tags)
 		if((playing === false) && ((tags.mod === true) || (tags.vip === true) || ((tags.username.toLowerCase() === "doctorsmooth")))){
 			playing = true;
 			client.say(channel,"!play");
